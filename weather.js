c59d681e94b452097cbba04a63c4ed5702ff2629
StarkOne <stark.pro40@gmail.com>
2022-08-17 16:54:21 +0700

    add helpers

---
+++ weather.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import { getArgs } from './helpers/args.js';
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  if (args.h) {
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+  }
+  console.log(args);
+};
+
+initCLI();
